f2b380e8dea3e4a37c048b3720ec73e2d6aa85a7
Fakhar Abbas Bhatti <fakharabbasbhatti333@gmail.com>
2025-11-21 14:53:09 +0500

    about page 3 file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cleaning from '../components/about/Cleaning'
 import OfferingsGuarantee from '../components/about/OfferingsGuarantee'
+import AuthorityCares from '../components/about/AuthorityCares'
 
 function About() {
   return (
     <div>
       <Cleaning/>
       <OfferingsGuarantee/>
+      <AuthorityCares/>
     </div>
   )
 }
