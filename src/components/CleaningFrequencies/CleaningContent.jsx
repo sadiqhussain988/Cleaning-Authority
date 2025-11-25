831d0859530663a3f9be0a5015ac02453c28ddac
muqadashayyat8@gmail.com <muqadashayyat8@gmail.com>
2025-11-25 15:56:31 +0500

    set the alignment of leaf in a card

--- src/components/CleaningFrequencies/CleaningContent.jsx
+++ src/components/CleaningFrequencies/CleaningContent.jsx
@@ -76,7 +76,7 @@ const CleaningContent = () => {
                 
                <div className="bg-blue-50 rounded-xl p-6 mb-6 border-l-4 border-[#0079C1] relative overflow-hidden">
   {/* Leaf decoration */}
- <div className="absolute top-2 right-4 opacity-80">
+ <div className="absolute top-1 right-4 opacity-70">
   <svg className="w-16 h-16" fill="#66a038" viewBox="0 0 24 24">
     <path d="M17 8C8 10 5.9 16.17 3.82 21.34L5.71 22L6.66 19.7C7.14 19.87 7.64 20 8 20C19 20 22 3 22 3C21 5 14 5.25 9 6.25C4 7.25 2 11.5 2 13.5C2 15.5 3.75 17.25 3.75 17.25C7 8 17 8 17 8Z"/>
   </svg>
